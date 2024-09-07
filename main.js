8add08f4d7c9da046016c349f43e6001de7e2f85
viktorKorneev <korneevviktor90@yandex.ru>
2024-09-07 19:25:14 +0300

    step-11

--- main.js
+++ main.js
@@ -4,50 +4,51 @@ fooButton.addEventListener("click", () => {
 });
 const foo = () => {
   const item = document.querySelectorAll(".item");
-  // console.log(item);
 
   const newArr = [...item];
 
   newArr.forEach((el) => {
     //debugger
-    return el.addEventListener("click", () => {
+    return (el.addEventListener("click", () => {
       el.classList.toggle("done");
-    });
-  });
-// };
+    })
+    )
+  })
+  
 
-// for(let i = 0; i < item.length; i++){
-// item[i].addEventListener("click", () =>{
-//     item[i].classList.toggle("done")
-// })
-// }
-}
-const form = document.querySelector(".form")
+  // for(let i = 0; i < item.length; i++){
+  // item[i].addEventListener("click", () =>{
+  //     item[i].classList.toggle("done")
+  // })
+  // }
+};
+
+foo()
+
+const form = document.querySelector(".form");
 form.addEventListener("submit", (event) => {
-    event.preventDefault()
-    const input = document.querySelector(".input")
-    const text = input.value
-    
-    const list = document.querySelector(".list")
-    // const newLi = document.createElement("li")
-    // // newLi.classList.add(".item")
-    // newLi.textContent = text
-    // list.append(newLi)
-    
-    const newTable = document.createElement("table")
-    const newTr = document.createElement("tr")
-    const newTd = document.createElement("td")
+  event.preventDefault();
+  const input = document.querySelector(".input");
+  let text = input.value;
 
-    newTd.textContent = text
-    newTr.append(newTd)
-    newTable.append(newTr)
-    list.append(newTable)
+  const list = document.querySelector(".list");
+  const newLi = document.createElement("li")
+  newLi.classList.add("item")
+  newLi.textContent = text
+  list.append(newLi)
+foo()
+//   const newTable = document.createElement("table");
+//   const newTr = document.createElement("tr");
+//   const newTd = document.createElement("td");
 
-    input.value = ""
-})
+//   newTd.textContent = text;
+//   newTr.append(newTd);
+//   newTable.append(newTr);
+//   list.append(newTable);
+
+  input.value = "";
+});
 // ---------------------------------------------------------------------------
-
-
 
 //  <table>
 //     <tr>
@@ -55,8 +56,7 @@ form.addEventListener("submit", (event) => {
 //             Иван
 //         </td>
 //     </tr>
-// </table> 
-
+// </table>
 
 // const fooButton = document.querySelector(".fooButton");
 // fooButton.addEventListener("click", () => {
@@ -86,7 +86,7 @@ form.addEventListener("submit", (event) => {
 //     event.preventDefault()
 //     const input = document.querySelector(".input")
 //     const text = input.value
-    
+
 //     const list = document.querySelector(".list")
 //     const newLi = document.createElement("li")
 //     // newLi.classList.add(".item")
