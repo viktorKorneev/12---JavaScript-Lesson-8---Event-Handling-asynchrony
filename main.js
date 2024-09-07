c991635235eed36a6b37c44713f84580604768cb
viktorKorneev <korneevviktor90@yandex.ru>
2024-09-07 13:55:57 +0300

    step-2

--- main.js
+++ main.js
@@ -1,21 +1,2 @@
-.list {
-    display: flex;
-    flex-direction: column;
-    align-items: start;
-    gap: 10px;
-  }
-   
-  .item {
-    font-family: Arial, sans-serif;
-    cursor: pointer;
-  }
-   
-  .done {
-    font-weight: 600; /* Делает текст более жирным, подчеркивая статус "пройдено" */
-  }
-   
-  .done::after {
-    content: "✅"; /* Добавляет знак галочки после текста элемента */
-    font-size: 13px;
-    margin-left: 5px;
-  }
+const item = document.querySelector(".item")
+console.log(item);
