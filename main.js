8cc98ac03399785ca07f0dcf7cad591d1391e380
viktorKorneev <korneevviktor90@yandex.ru>
2024-09-07 15:41:22 +0300

    step-9

--- main.js
+++ main.js
@@ -3,33 +3,38 @@
 //   foo();
 // });
 // const foo = () => {
-  const item = document.querySelectorAll(".item");
-  // console.log(item);
+//   const item = document.querySelectorAll(".item");
+//   // console.log(item);
 
-  const newArr = [...item];
+//   const newArr = [...item];
 
-  newArr.forEach((el) => {
-    //debugger
-    return el.addEventListener("click", () => {
-      el.classList.toggle("done");
-    });
-  });
-// };
+//   newArr.forEach((el) => {
+//     //debugger
+//     return el.addEventListener("click", () => {
+//       el.classList.toggle("done");
+//     });
+//   });
+// // };
 
-// for(let i = 0; i < item.length; i++){
-// item[i].addEventListener("click", () =>{
-//     item[i].classList.toggle("done")
-// })
-// }
-const form = document.querySelector(".form")
-form.addEventListener("submit", (event) => {
-    event.preventDefault()
-    const input = document.querySelector(".input")
-    const text = input.value
+// // for(let i = 0; i < item.length; i++){
+// // item[i].addEventListener("click", () =>{
+// //     item[i].classList.toggle("done")
+// // })
+// // }
+// const form = document.querySelector(".form")
+// form.addEventListener("submit", (event) => {
+//     event.preventDefault()
+//     const input = document.querySelector(".input")
+//     const text = input.value
     
-    const list = document.querySelector(".list")
-    const newLi = document.createElement("li")
-    newLi.classList.add(".item")
-    newLi.textContent = text
-    list.append(newLi)
-})
+//     const list = document.querySelector(".list")
+//     const newLi = document.createElement("li")
+//     // newLi.classList.add(".item")
+//     newLi.textContent = text
+//     list.append(newLi)
+
+//     input.value = ""
+// })
+// ---------------------------------------------------------------------------
+
+
