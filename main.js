589d830614813a69aceeeb8b1bec840417a62e72
viktorKorneev <korneevviktor90@yandex.ru>
2024-09-07 15:56:51 +0300

    step-10

--- main.js
+++ main.js
@@ -1,3 +1,63 @@
+const fooButton = document.querySelector(".fooButton");
+fooButton.addEventListener("click", () => {
+  foo();
+});
+const foo = () => {
+  const item = document.querySelectorAll(".item");
+  // console.log(item);
+
+  const newArr = [...item];
+
+  newArr.forEach((el) => {
+    //debugger
+    return el.addEventListener("click", () => {
+      el.classList.toggle("done");
+    });
+  });
+// };
+
+// for(let i = 0; i < item.length; i++){
+// item[i].addEventListener("click", () =>{
+//     item[i].classList.toggle("done")
+// })
+// }
+}
+const form = document.querySelector(".form")
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const input = document.querySelector(".input")
+    const text = input.value
+    
+    const list = document.querySelector(".list")
+    // const newLi = document.createElement("li")
+    // // newLi.classList.add(".item")
+    // newLi.textContent = text
+    // list.append(newLi)
+    
+    const newTable = document.createElement("table")
+    const newTr = document.createElement("tr")
+    const newTd = document.createElement("td")
+
+    newTd.textContent = text
+    newTr.append(newTd)
+    newTable.append(newTr)
+    list.append(newTable)
+
+    input.value = ""
+})
+// ---------------------------------------------------------------------------
+
+
+
+//  <table>
+//     <tr>
+//         <td>
+//             Иван
+//         </td>
+//     </tr>
+// </table> 
+
+
 // const fooButton = document.querySelector(".fooButton");
 // fooButton.addEventListener("click", () => {
 //   foo();
@@ -35,6 +95,3 @@
 
 //     input.value = ""
 // })
-// ---------------------------------------------------------------------------
-
-
