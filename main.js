70debc6aafd543614e69232d72e7e655bc7312c9
viktorKorneev <korneevviktor90@yandex.ru>
2024-09-07 15:00:27 +0300

    step-5

--- main.js
+++ main.js
@@ -1,22 +1,19 @@
-const item = document.querySelectorAll(".item")
-// console.log(item);
+const foo = () => {
+  const item = document.querySelectorAll(".item");
+  // console.log(item);
 
-const newArr = [...item]
-newArr.map(el=> {
+  const newArr = [...item];
+
+  newArr.forEach((el) => {
     //debugger
-    return (
-        el.addEventListener("click", ()=> {
-            el.classList.toggle("done")
-        })
-    )
-})
-
-
+    return el.addEventListener("click", () => {
+      el.classList.toggle("done");
+    });
+  });
+};
 
 // for(let i = 0; i < item.length; i++){
 // item[i].addEventListener("click", () =>{
 //     item[i].classList.toggle("done")
 // })
 // }
-
-
