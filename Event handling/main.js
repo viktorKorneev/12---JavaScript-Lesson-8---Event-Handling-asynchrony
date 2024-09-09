a40f60780e109178f00bc4425f08edb1d1f6f2da
viktorKorneev <korneevviktor90@yandex.ru>
2024-09-09 20:31:48 +0300

    step-7

--- Event handling/main.js
+++ Event handling/main.js
@@ -4,46 +4,14 @@
 
 window.onload = init;
 function init() {
-  let image0 = document.getElementById("zero");
-  image0.onclick = showImageZero;
-  let image1 = document.getElementById("one");
-  image1.onclick = showImageOne;
-  let image2 = document.getElementById("two");
-  image2.onclick = showImageTwo;
-  let image3 = document.getElementById("three");
-  image3.onclick = showImageThree;
-  let image4 = document.getElementById("four");
-  image4.onclick = showImageFour;
-  let image5 = document.getElementById("five");
-  image5.onclick = showImageFive;
+  let images = document.getElementsByTagName("img");
+  for(let i = 0; i < images.length; i++){
+    images[i].onclick = showAnswer;
+  }
 }
 
-function showImageZero() {
-  let image = document.getElementById("zero");
-  image.src = "zero.jpg";
+function showAnswer() {
+    
 }
 
-function showImageOne() {
-  let image = document.getElementById("one");
-  image.src = "one.jpg";
-}
 
-function showImageTwo() {
-  let image = document.getElementById("two");
-  image.src = "two.jpg";
-}
-
-function showImageThree() {
-  let image = document.getElementById("three");
-  image.src = "three.jpg";
-}
-
-function showImageFour() {
-  let image = document.getElementById("four");
-  image.src = "four.jpg";
-}
-
-function showImageFive() {
-  let image = document.getElementById("five");
-  image.src = "five.jpg";
-}
