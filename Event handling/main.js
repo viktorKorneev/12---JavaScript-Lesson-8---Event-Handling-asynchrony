fb93cf8ae3213c81ee1af3690c21dec78c89fb37
viktorKorneev <korneevviktor90@yandex.ru>
2024-09-10 22:26:22 +0300

    step-9

--- Event handling/main.js
+++ Event handling/main.js
@@ -15,4 +15,12 @@ function showAnswer(eventObj) {
   let name = image.id;
   name += ".jpg";
   image.src = name;
+
+  setTimeout(reblur, 2000, image);
+}
+
+function reblur (image) {
+  let name = image.id;
+  name = name + "blur.jpg";
+  image.src = name;
 }
